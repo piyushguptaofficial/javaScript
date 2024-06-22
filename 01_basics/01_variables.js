315860d447160813341b68d3924f4c2a3452e568
piyushguptaofficial <100441111+piyushguptaofficial@users.noreply.github.com>
2024-06-22 05:33:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newwer version
+
+
+// // variables in javascript
+// const accountId = 144553
+// let accountEmail = "random@public.com"
+// var accountPassword = "112233"
+// accountCity = "dhankur"
+
+// console.log(accountId);
+// console.table([accountEmail,accountId,accountPassword,accountCity])
+// // alert(3 + 3) using nodeJs, not browser
+
+// console.log(typeof accountId);
